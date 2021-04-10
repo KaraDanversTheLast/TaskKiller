f3dca8cd46e937eb7a17942856abe8d6ada7b3ae
KaraDanversTheLast <hentaiearthchan@mail.ru>
2021-04-10 20:28:55 +1200

    Добавьте файлы проекта.

---
+++ TaskKiller/TaskKiller.cpp
@@ -0,0 +1,134 @@
+﻿/*
+	@~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#@
+	@										TaskKiller									 @
+	@																					 @
+	@					Программа завершает все указанные пользователем процессы.	     @
+	@																					 @
+	@~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#@
+*/
+
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+
+	 
+	system("color 4"); // Цвет текста консоли
+
+
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im  /f");
+	system("taskkill /im GenshinImpact.exe /f");
+	system("taskkill /im javaw.exe /f");
+	system("taskkill /im 7zFM.exe /f");
+	system("taskkill /im AdobeIPCBroker.exe /f");
+	system("taskkill /im AIMP.exe /f");
+	system("taskkill /im AutoClicker.en.exe /f");
+	system("taskkill /im AutoClicker.exe /f");
+	system("taskkill /im cheatengine-x86_64-SSE4-AVX2.exe /f");
+	system("taskkill /im Code.exe /f");
+	system("taskkill /im csgo.exe /f");
+	system("taskkill /im CustomizerGod.exe /f");
+	system("taskkill /im DesktopIconSaver.exe /f");
+	system("taskkill /im Discord.exe /f");
+	system("taskkill /im draw.io.exe /f");
+	system("taskkill /im DropIt.exe /f");
+	system("taskkill /im EpicGamesLauncher.exe /f");
+	system("taskkill /im Everything.exe /f");
+	system("taskkill /im FACEIT.exe /f");
+	system("taskkill /im faceitclient.exe /f");
+	system("taskkill /im firefox.exe /f");
+	system("taskkill /im fraps_RUS.exe /f");
+	system("taskkill /im fraps.exe /f");
+	system("taskkill /im fraps64.dat /f");
+	system("taskkill /im gdbus.exe /f");
+	system("taskkill /im GitHubDesktop.exe /f");
+	system("taskkill /im GoogleCrashHandler.exe /f");
+	system("taskkill /im GoogleCrashHandler64.exe /f");
+	system("taskkill /im googledrivesync.exe /f");
+	system("taskkill /im GoogleUpdate.exe /f");
+	system("taskkill /im HiSuite.exe /f");
+	system("taskkill /im HuaweiHiSuiteService64.exe /f");
+	system("taskkill /im image-map.exe /f");
+	system("taskkill /im inkscape.exe /f");
+	system("taskkill /im Koikatu.exe /f");
+	system("taskkill /im launcher.exe /f");
+	system("taskkill /im MacroRecorder.exe /f");
+	system("taskkill /im MinecraftLauncher.exe /f");
+	system("taskkill /im msedge.exe /f");
+	system("taskkill /im notepad++.exe /f");
+	system("taskkill /im Nox.exe /f");
+	system("taskkill /im OE7.exe /f");
+	system("taskkill /im PaintDotNet.exe /f");
+	system("taskkill /im Photoshop.exe /f");
+	system("taskkill /im PicasaPhotoViewer.exe /f");
+	system("taskkill /im PotPlayerMini64.exe /f");
+	system("taskkill /im RegOrganizer.exe /f");
+	system("taskkill /im ScpDriverInstaller.exe /f");
+	system("taskkill /im ScpMonitor.exe /f");
+	system("taskkill /im ScpSettings.exe /f");
+	system("taskkill /im ScpUpdater.exe /f");
+	system("taskkill /im SkinInstaller.exe /f");
+	system("taskkill /im SoftwareUpdater.exe /f");
+	system("taskkill /im steam.exe /f");
+	system("taskkill /im SWXDAgent.exe /f");
+	system("taskkill /im Telegram.exe /f");
+	system("taskkill /im TreeSize.exe /f");
+	system("taskkill /im TreeSizeFree.exe /f");
+	system("taskkill /im utweb.exe /f");
+	system("taskkill /im wallpaper32.exe /f");
+	system("taskkill /im wallpaperservice32_c.exe /f");
+	system("taskkill /im WinRAR.exe /f");
+	system("taskkill /im WSHelper.exe /f");
+	system("taskkill /im YourPhone.exe /f");
+	system("taskkill /im Zona.exe /f");
+	system("taskkill /im cmd.exe /f");
+
+
+	// system("cls"); // Очистка консоли
+	
+	system("color a");
+
+	cout << "@~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#@" << endl;
+	cout << "																				      " << endl;
+	cout << "								Congratulation!" << endl;
+	cout << "																					  " << endl;
+	cout << "@~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#~~~~~#@" << endl;
+
+	// system("pause"); // Не закрывает консоль после завершения процесса
+
+	return 0;
+}
